a11d116d6a7d23ba7c942076419a241abcff2418
oldmandev17 <127681749+oldmandev17@users.noreply.github.com>
2023-10-23 18:20:41 +0700

    update layout auth

--- admin/src/components/auth/FooterAuth.tsx
+++ admin/src/components/auth/FooterAuth.tsx
@@ -1,6 +1,6 @@
 function FooterAuth() {
   return (
-    <footer className='bg-white rounded-lg shadow sm:flex sm:items-center sm:justify-between p-4 sm:p-6 xl:p-8 dark:bg-gray-800 antialiased'>
+    <footer className='bg-white rounded-lg shadow sm:flex sm:items-center sm:justify-between p-4 sm:p-6 xl:p-8 dark:bg-gray-800 antialiased fixed bottom-0 left-0 right-0'>
       <p className='mb-4 text-sm text-center text-gray-500 dark:text-gray-400 sm:mb-0'>
         &copy; 2019-2022{' '}
         <a href='https://flowbite.com/' className='hover:underline' target='_blank' rel='noreferrer'>
